7ade40445a3acd5196d062e0487bd14558db40a1
Fsher07 <furkansalimm@gmail.com>
2022-02-28 20:07:30 +0300

    Fix js function and add opening menu properties
    -fixed fucnction not working due to typo
    -added background color, paddings, font sizes for opening menu

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-const menu = document.querySelector('nav-menu');
-const navbarItems = document.querySelectorAll('.navbar-item');
+const menu = document.querySelector('.desktop-nav');
+const navbarItems = document.querySelectorAll('.navbarItem');
 const hamburgerMenu = document.querySelector('#hamburgerMenu');
 const menuIcon = document.querySelector('#menuIcon');
-const closeIcon = document.querySelector('#closeIcon');
+const closeIcon = document.querySelector("#closeIcon");
 
 function toggleMenu() {
-  if (menu.classList.contains('.openMenu')) {
+  if (menu.classList.contains('openMenu')) {
     menu.classList.remove('openMenu');
-    menuIcon.classList.display('block');
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
   } else {
     menu.classList.add('openMenu');
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
   }
 }
 
 hamburgerMenu.addEventListener("click", toggleMenu);
+
+navbarItems.forEach(
+  function(navbarItems) {
+    navbarItems.addEventListener("click", toggleMenu);
+  }
+)
