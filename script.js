a2188201a2ee4c94504eb3929976a798e3e8440e
Fsher07 <furkansalimm@gmail.com>
2022-03-01 22:35:24 +0300

    Uptade css to create a color class

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Popup mobile navbar menu
 const menu = document.querySelector('.desktop-nav');
-const navbarItems = document.querySelectorAll('.navbarItem');
-const hamburgerMenu = document.querySelector('#hamburgerMenu');
-const menuIcon = document.querySelector('#menuIcon');
+const navbarItems = document.querySelectorAll('.navbarItem'); // Get all the navbar items
+const hamburgerMenu = document.querySelector('#hamburgerMenu'); 
+const menuIcon = document.querySelector('#menuIcon'); 
 const closeIcon = document.querySelector("#closeIcon");
 
 function toggleMenu() {
@@ -21,5 +22,24 @@ hamburgerMenu.addEventListener("click", toggleMenu);
 navbarItems.forEach(
   function(navbarItems) {
     navbarItems.addEventListener("click", toggleMenu);
+  }
+)
+
+// Popup desktop project window
+const projectWindow = document.querySelector('.popup-window');
+const projectOpenButton = document.querySelectorAll('.popup-window-button');
+const projectCloseButton = document.querySelector('.project-closeIcon');
+
+function openProjectWindow () {
+ if (projectWindow.classList.contains('active')) {
+    projectWindow.classList.remove('active');
+ }else {
+    projectWindow.classList.add('active');
+ }
+}
+
+projectOpenButton.forEach(
+  function(projectOpenButton) {
+    projectOpenButton.addEventListener("click", openProjectWindow);
   }
 )
