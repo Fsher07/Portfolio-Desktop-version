0576a161150c41712d8737d6727a425c303d806b
Fsher07 <furkansalimm@gmail.com>
2022-03-04 12:09:24 +0300

    Remove static elements for project cards
    -removed all static elements for project cards
    -created project cards dynamically with js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const projectWindows = [
   {
     title: 'Data Dashboard Healthcare',
     technologies: 'Bootstrap html Google',
-    image: 'images/portfolioPng.png',
+    image: 'images/placeholder.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industrys standard',
     liveLink: '#',
     sourceLink: '#',
@@ -45,7 +45,7 @@ const projectWindows = [
   {
     title: 'Data Dashboard Healthcare',
     technologies: 'Bootstrap html Google',
-    image: 'images/portfolioPng.png',
+    image: 'images/card2.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industrys standard',
     liveLink: '#',
     sourceLink: '#',
@@ -53,7 +53,7 @@ const projectWindows = [
   {
     title: 'Data Dashboard Healthcare',
     technologies: 'Bootstrap html Google',
-    image: 'images/portfolioPng.png',
+    image: 'images/card3.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industrys standard',
     liveLink: '#',
     sourceLink: '#',
@@ -61,7 +61,7 @@ const projectWindows = [
   {
     title: 'Data Dashboard Healthcare',
     technologies: 'Bootstrap html Google',
-    image: 'images/portfolioPng.png',
+    image: 'images/workbg.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industrys standard',
     liveLink: '#',
     sourceLink: '#',
@@ -69,7 +69,7 @@ const projectWindows = [
   {
     title: 'Data Dashboard Healthcare',
     technologies: 'Bootstrap html Google',
-    image: 'images/portfolioPng.png',
+    image: 'images/card5.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industrys standard',
     liveLink: '#',
     sourceLink: '#',
@@ -77,11 +77,39 @@ const projectWindows = [
   {
     title: 'Data Dashboard Healthcare',
     technologies: 'Bootstrap html Google',
-    image: 'images/portfolioPng.png',
+    image: 'images/card3.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.has been the industrys standard',
     liveLink: '#',
     sourceLink: '#',
   }];
+
+// project card 1 is created seperately because it is different from the other 5 cards
+function projectCard1() {
+  return `<div class="card card1 ProjectPage" style="background-image: url('${projectWindows[1].image}');">
+  <button type="button" class="projectbtn btn project-open-button">See Project</button>`;
+}
+
+// other project cards
+function projectCardContent(info) {
+  return `<div class="card ProjectPage" style="background-image: url('${info.image}');">
+  <h3 class="project-titles ProjectPage-h2">${info.title}</h3>
+  <p>${info.description}</p>
+  <ul class="project-ul">
+    <li><a class="Tags" href="#">${info.technologies.split(' ')[0]}</a></li>
+    <li><a class="Tags" href="#">${info.technologies.split(' ')[1]}</a></li>
+    <li><a class="Tags" href="#">${info.technologies.split(' ')[2]}</a></li>
+  </ul>
+  <button type="button" class="projectbtn btn none-btn">See Project</button>
+</div>`;
+}
+
+const projectCardsContainer = document.querySelector('.works-projects');
+if (projectCardsContainer) {
+  projectCardsContainer.innerHTML += projectCard1();
+  for (let i = 2; i < projectWindows.length; i += 1) {
+    projectCardsContainer.innerHTML += projectCardContent(projectWindows[i]);
+  }
+}
 
 const openButton = document.querySelectorAll('.project-open-button');
 function popupProjectWindow() {
@@ -161,29 +189,3 @@ function popupProjectWindow() {
 openButton.forEach((button) => {
   button.addEventListener('click', popupProjectWindow);
 });
-
-function projectCard1() { // project card 1 is created seperately because it is different from the other 5 cards
-  return `<div class="card card1 ProjectPage">
-  <button type="button" class="projectbtn btn project-open-button">`  
-}
-
-function projectCardContent(info) {
-  return `<div class="card ProjectPage-h2">
-  <h3 class="project-titles ProjectPage-h2">${info.title}</h3>
-  <p>${info.description}</p>
-  <ul class="project-ul">
-    <li><a class="Tags" href="#">${info.technologies.split(' ')[0]}</a></li>
-    <li><a class="Tags" href="#">${info.technologies.split(' ')[1]}</a></li>
-    <li><a class="Tags" href="#">${info.technologies.split(' ')[2]}</a></li>
-  </ul>
-</div>`;
-}
-
-const projectCardsContainer = document.querySelector('.works-projects');
-if (projectCardsContainer) {
-  projectCard1();
-  for (let i = 2; i < projectWindows.length; i++) {
-    projectCardsContainer.innerHTML += projectCardContent(projectWindows[i]);
-  }
-}  
-
